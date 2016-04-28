ffce69a4af37ce68dd8cd011736852f04bb625c5
Rafa Poveda <raven@raven.es>
2016-04-28 11:31:25 +0200

    get rid of 'new' elements in left sidebar

--- js/publisher_profile.js
+++ js/publisher_profile.js
@@ -5,3 +5,5 @@ document.getElementById("formatdiv").innerHTML = "";
 document.getElementById("categorydiv").innerHTML = "";
 document.getElementById("tagsdiv-post_tag").innerHTML = "";
 document.getElementById("title").disabled=true;
+jQuery( "#menu-posts ul li:nth-child(3)" ).html("");
+jQuery( "#menu-pages ul li:nth-child(3)" ).html("");
