7cfbdc6db9f15049e878a2ea8af2efe065dc6a94
Rafa Poveda <raven@raven.es>
2016-04-28 07:41:07 +0200

    changing name of the profile to publisher_profile

--- publisher-profile.php
+++ publisher-profile.php
@@ -10,12 +10,17 @@
 */
 
 
-// We add the publisher role with level 0
+// We add the publisher role with a Contributor level
 function publisher_profile_add_publisher_role() {
-  add_role( 'publisher', 'Publisher', array( 
-    'level_0' => true,
+
+add_role('publisher_profile', 'Publisher', array( 
+    'read' => true, // True allows that capability
+    'edit_posts' => true,
+    'delete_posts' => false, // Use false to explicitly deny
   ) );
+
 }
+
 add_action('admin_init', 'publisher_profile_add_publisher_role');
 
 
@@ -23,7 +28,7 @@ add_action('admin_init', 'publisher_profile_add_publisher_role');
 // We add the capabilities - edit and publish
 function publisher_profile_add_caps() {
     // gets the publisher role
-    $role = get_role( 'publisher' );
+    $role = get_role( 'publisher_profile' );
 
     // This only works, because it accesses the class instance.
     // would allow the publisher to edit others' posts for current theme only
@@ -39,7 +44,7 @@ add_action( 'admin_init', 'publisher_profile_add_caps');
 
 function publisher_profile_launch(){
     $user = wp_get_current_user();
-    if ( in_array( 'publisher', (array) $user->roles ) ) {
+    if ( in_array( 'publisher_profile', (array) $user->roles ) ) {
       add_filter( 'tiny_mce_before_init', 'publisher_profile_format_TinyMCE' );
     }
 }
@@ -49,7 +54,7 @@ add_filter( 'admin_init', 'publisher_profile_launch' );
 // We make tinyMCE read-only for the publishers
 function publisher_profile_format_TinyMCE( $in ) {
   $user = wp_get_current_user();
-    if ( in_array( 'publisher', (array) $user->roles ) ) {
+    if ( in_array( 'publisher_profile', (array) $user->roles ) ) {
       $in['readonly'] = 1;
       return $in;
     }
@@ -61,7 +66,7 @@ add_filter( 'wp_default_editor', create_function('', 'return "tinymce";') );
 // We disable the HTML tag for publishers - publishers can only use the visual view, which is blocked
 function publisher_profile_disable_html() {
   $user = wp_get_current_user();
-  if ( in_array( 'publisher', (array) $user->roles ) ) {
+  if ( in_array( 'publisher_profile', (array) $user->roles ) ) {
    echo '<style type="text/css">#content-html, #quicktags {display: none;}</style>' . "\n";
   }
 }
