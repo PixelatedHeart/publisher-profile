c8441e99de2277a960296ce73f84ce740a6f2fe9
Rafa Poveda <raven@raven.es>
2016-04-28 16:37:06 +0200

    change of order when executing jQuery

--- js/publisher_profile.js
+++ js/publisher_profile.js
@@ -1,9 +1,9 @@
 //Don't show elements in screen
 
+jQuery( "#menu-posts ul li:nth-child(3)" ).html("");
+jQuery( "#menu-pages ul li:nth-child(3)" ).html("");
 document.getElementById("normal-sortables").innerHTML = "";
 document.getElementById("formatdiv").innerHTML = "";
 document.getElementById("categorydiv").innerHTML = "";
 document.getElementById("tagsdiv-post_tag").innerHTML = "";
 document.getElementById("title").disabled=true;
-jQuery( "#menu-posts ul li:nth-child(3)" ).html("");
-jQuery( "#menu-pages ul li:nth-child(3)" ).html("");
