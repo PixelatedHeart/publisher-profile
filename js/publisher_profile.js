2de0502209927bf167f75465d8ce1bbbfa61416f
Rafa Poveda <raven@raven.es>
2016-04-29 06:36:18 +0200

    changing order element

--- js/publisher_profile.js
+++ js/publisher_profile.js
@@ -2,8 +2,8 @@
 
 jQuery( "#menu-posts ul li:nth-child(3)" ).html("");
 jQuery( "#menu-pages ul li:nth-child(3)" ).html("");
+document.getElementById("title").disabled=true;
 document.getElementById("normal-sortables").innerHTML = "";
 document.getElementById("formatdiv").innerHTML = "";
 document.getElementById("categorydiv").innerHTML = "";
 document.getElementById("tagsdiv-post_tag").innerHTML = "";
-document.getElementById("title").disabled=true;
